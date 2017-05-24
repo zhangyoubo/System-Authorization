8ddb871565db2e6bfdc774ad3aff6ec74b86ef03
zhangyoubo <13818956024@163.com>
2017-05-24 09:56:40 +0800

    Initial Commit

---
+++ System Authorization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  System Authorization
+//
+//  Created by 张友波 on 2017/5/24.
+//  Copyright © 2017年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ System Authorization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  System Authorization
+//
+//  Created by 张友波 on 2017/5/24.
+//  Copyright © 2017年 张友波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
